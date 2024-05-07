61fadebd59fa09c7db886f3df2bf3b4b5ce900cf
norhanhatem0187 <norhan.hatem0187@gmail.com>
2024-05-07 16:55:52 +0300

    Update deployOnKubernetes.groovy

--- vars/deployOnKubernetes.groovy
+++ vars/deployOnKubernetes.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
 // KubernetesCredentialsID 'KubeConfig file'
-def call(String k8sCredentialsID, String branchName) {
+def call(String k8sCredentialsID ) {
 
     // login to k8s Cluster via KubeConfig file
     withCredentials([file(credentialsId: "${k8sCredentialsID}", variable: 'KUBECONFIG_FILE')]) {
-        sh "export KUBECONFIG=${KUBECONFIG_FILE} && kubectl apply -f . --namespace=${branchName}"
+        sh "export KUBECONFIG=${KUBECONFIG_FILE} && kubectl apply -f . "
     }
 }
 
